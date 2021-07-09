a8b0fa3d398ba76386fdc0e115ef7ae25b9a1dbd
ditdittdittt <ditdittdittt@gmail.com>
2021-07-09 20:10:25 +0700

    add indexes

--- repository/pgsql/courier_cost_mapping.go
+++ repository/pgsql/courier_cost_mapping.go
@@ -5,8 +5,8 @@ type CourierCostMapping struct {
 	CourierID               int      `json:"courier_id"`
 	Courier                 *Courier `json:"courier"`
 	ServiceName             string   `json:"service_name"`
-	PostcodeCitySource      string   `json:"postcode_city_source"`
-	PostcodeCityDestination string   `json:"postcode_city_destination"`
+	PostcodeCitySource      string   `gorm:"index:idx_member" json:"postcode_city_source"`
+	PostcodeCityDestination string   `gorm:"index:idx_member" json:"postcode_city_destination"`
 	BasePrice               int      `json:"base_price"`
 	CoefWeight              float64  `json:"coef_weight"`
 }
